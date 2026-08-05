06e19c7bb484004d2fad157232d1d83e7829a726
davelee2804 <davelee2804@gmail.com>
2026-08-05 10:44:26 +1000

    fixing some formatting issues in the spectra

--- src/scores/dynamics/spectra_impl.py
+++ src/scores/dynamics/spectra_impl.py
@@ -68,22 +68,26 @@ def power_spectra(
         L = np.pi * constants.RAD_EARTH / 180.0 * n_lon * d_theta
         if L < 2.0 * np.pi * constants.RAD_EARTH - 1.0e-6:
             # regional domain, apply cosine bell filtering
-	    error_msg = ValueError(f"The zonal_filter_width is: {zonal_filter_with}, must be < 0.5.")
-	    if zonal_filter_width >= 0.5:
-		    raise error_msg
+            error_msg = ValueError(f"The zonal_filter_width is: {zonal_filter_with}, must be < 0.5.")
+            if zonal_filter_width >= 0.5:
+                raise error_msg
 
-	    theta_l = np.min(K.longitude.values)
-	    theta_r = theta_l + L
-	    filter_l = 0.5 * (1.0 - np.cos(np.pi * (K.longitude.values - theta_l) / (zonal_filter_width * L)))
-	    filter_r = 0.5 * (1.0 - np.cos(np.pi * (theta_r - K.longitude.values) / (zonal_filter_width * L)))
-	    mask_l = K.longitude.values < theta_l + zonal_filter_width * L
-	    mask_r = K.longitude.values > theta_r - zonal_filter_width * L
-	    filter_1d = np.ones(n_lon)
-	    filter_1d[mask_l] = filter_l
-	    filter_1d[mask_r] = filter_r
-	    filter_2d = np.ones((nlat, 1)) * filter_1d[None, :]
-	    filter_xr = xr.DataArray(filter_2d, dims=[latitude_name, longitude_name], coords={latitude_name: K.latitude, longitude_name: K.longitude})
-	    K = filter_xr * K
+            theta_l = np.min(K.longitude.values)
+            theta_r = theta_l + L
+            filter_l = 0.5 * (1.0 - np.cos(np.pi * (K.longitude.values - theta_l) / (zonal_filter_width * L)))
+            filter_r = 0.5 * (1.0 - np.cos(np.pi * (theta_r - K.longitude.values) / (zonal_filter_width * L)))
+            mask_l = K.longitude.values < theta_l + zonal_filter_width * L
+            mask_r = K.longitude.values > theta_r - zonal_filter_width * L
+            filter_1d = np.ones(n_lon)
+            filter_1d[mask_l] = filter_l
+            filter_1d[mask_r] = filter_r
+            filter_2d = np.ones((nlat, 1)) * filter_1d[None, :]
+            filter_xr = xr.DataArray(
+                    filter_2d, 
+                    dims=[latitude_name, longitude_name], 
+                    coords={latitude_name: K.latitude, longitude_name: K.longitude}
+                )
+            K = filter_xr * K
 
         lon_index = K.get_index(longitude_name)
         fft_lon = xr.apply_ufunc(
@@ -91,13 +95,13 @@ def power_spectra(
                 K,
                 input_core_dims=[[longitude_name]],
                 output_core_dims=[["wavenumber"]],
-                kwargs={index: lon_index, norm="forward"},
-        )
+                kwargs={index: lon_index, norm: "forward"},
+            )
 
         # ensure that the wavelengths are consistent for each latitude
         cos_theta_inv = 1.0 / np.cos(K.latitude.values)
         equator_freq = np.fft.rttffreq(n_lon, L / 2.0 / np.pi)
         freq_2d = cos_theta_inv[:, None] * equator_freq[None, :]
-        fft_lon.["frequency"] = (["waqvenumber", latitude_name], freq_2d)
+        fft_lon["frequency"] = (["waqvenumber", latitude_name], freq_2d)
 
         return fft_lon
